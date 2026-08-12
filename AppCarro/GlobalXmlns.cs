9a66beb3e4b04a16ba9f44f2225f4b943a577199
Leonardo Miguel Trindade <trindadeleo02@gmail.com>
2026-08-12 06:32:17 -0300

    Add project files.

---
+++ AppCarro/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppCarro")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppCarro.Pages")]
---
+++ AppCarro/MainPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿namespace AppCarro
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //função que chama o método OnCalcularClicked quando o botão é clicado
+        private void OnCalcularClicked(object? sender, EventArgs e)
+        {
+            //Conversão dos valores inseridos pelo usuário para double, e atrubuição de preenchimento estão preenchidos true = preenchido, false = não preenchido
+            bool precoAlcoolTry = double.TryParse(EntryPrecoAlcool.Text, out double precoAlcool);
+            bool precoGasolinaTry = double.TryParse(EntryPrecoGasolina.Text, out double precoGasolina);
+            bool quilometragemInicialTry = double.TryParse(EntryQuilometragemInicial.Text, out double quilometragemInicial);
+            bool quilometragemFinalTry = double.TryParse(EntryQuilometragemFinal.Text, out double quilometragemFinal);
+            bool litrosAbastecidosTry = double.TryParse(EntryLitrosAbastecidos.Text, out double litrosAbastecidos);
+            bool consumoEstimadoTry = double.TryParse(EntryConsumoEstimado.Text, out double consumoEstimado);
+
+            //Verificação se todos os campos foram preenchidos
+            if (!precoAlcoolTry || !precoGasolinaTry || !quilometragemInicialTry || !quilometragemFinalTry || !litrosAbastecidosTry || !consumoEstimadoTry || PickerCombustivel.SelectedItem is not string combustivelSelecionado)
+            {
+                DisplayAlert("Erro", "Por favor, insira valores válidos em todos os campos.", "OK");
+
+            }
+            //Verificação se a quilometragem final é menor que a inicial (Se não quebraria toda a lógica do sistema)
+            else if (quilometragemFinal < quilometragemInicial)
+            {
+                DisplayAlert("Erro", "A quilometragem final não pode ser menor que a inicial.", "OK");
+
+            }
+            //Verificação se a quilometragem final é igual a inicial (Se não quebraria toda a lógica do sistema também)
+            else if (quilometragemFinal == quilometragemInicial)
+            {
+                DisplayAlert("Erro", "A quilometragem final não pode ser igual à inicial.", "OK");
+            }
+            //Verificação se a quantidade de litros abastecidos é menor ou igual a 0 (Sem ele não tem como fazer os cálculos principais)
+            else if (litrosAbastecidos <= 0)
+            {
+                DisplayAlert("Erro", "A quantidade de litros abastecidos deve ser um valor positivo.", "OK");
+            }
+            //Após todas as verificações, o sistema realiza os cálculos de consumo e custo por km, e exibe os resultados na tela
+            else
+            {
+
+                //Cálculo da distância percorrida
+                double distanciapercorrida = quilometragemFinal - quilometragemInicial;
+
+                //Cálculo da regra dos 70%
+                double regrados70 = precoAlcool / precoGasolina;
+
+                //Declaração das variáveis de consumo e custo
+                double consumoAlcool;
+                double consumoGasolina;
+                double custoAlcool;
+                double custoGasolina;
+
+                //Verificação do combustível selecionado
+                if (combustivelSelecionado == "Álcool")
+                {
+                    //Calculo do consumo e custo do álcool
+                    consumoAlcool = distanciapercorrida / litrosAbastecidos;
+
+                    //Atrubuição do consumo estimado da gasolina (Valor estimado pelo usuário)
+                    consumoGasolina = consumoEstimado;
+
+                    //Consumo e custo por km do álcool e gasolina
+                    custoAlcool = precoAlcool / consumoAlcool;
+                    custoGasolina = precoGasolina / consumoGasolina;
+
+                    //Exibição dos resultados na tela
+                    LabelResultadoConsumoAlcool.Text = "Consumo Alcool: " + consumoAlcool.ToString("F2") + " km/l";
+                    LabelResultadoConsumoGasolina.Text = "Consumo Gasolina: " + consumoGasolina.ToString("F2") + "km/l";
+                    LabelResultadoCustoAlcool.Text = "Custo Alcool: " + custoAlcool.ToString("F2") + " R$/km";
+                    LabelResultadoCustoGasolina.Text = "Custo Gasolina: " + custoGasolina.ToString("F2") + " R$/km";
+                }
+                //Se o combustível selecionado for gasolina
+                else
+                {
+                    //Cálculo do consumo da gasolina
+                    consumoGasolina = distanciapercorrida / litrosAbastecidos;
+
+                    //Atribuição do consumo estimado do álcool (Valor estimado pelo usuário)
+                    consumoAlcool = consumoEstimado;
+
+                    //Cálculo do custo por km do álcool
+                    custoAlcool = precoAlcool / consumoAlcool;
+
+                    //Cálculo do custo por km da gasolina
+                    custoGasolina = precoGasolina / consumoGasolina;
+
+                    //Exibição dos resultados na tela
+                    LabelResultadoConsumoAlcool.Text = "Consumo Alcool: " + consumoAlcool.ToString("F2") + " km/l";
+                    LabelResultadoConsumoGasolina.Text = "Consumo Gasolina: " + consumoGasolina.ToString("F2") + " km/l";
+                    LabelResultadoCustoAlcool.Text = "Custo Alcool: " + custoAlcool.ToString("F2") + " R$/km";
+                    LabelResultadoCustoGasolina.Text = "Custo Gasolina: " + custoGasolina.ToString("F2") + " R$/km";
+                }
+
+
+                //Verificação de qual combustível é mais custobenefício
+                if (custoAlcool <= custoGasolina)
+                {
+                    LabelResultadoCombustivel.Text = "É mais Custobenefício abastecer com Álcool.";
+                }
+                else
+                {
+                    LabelResultadoCombustivel.Text = "É mais Custobenefício abastecer com Gasolina.";
+                }
+
+                //Verificação de qual combustível é mais custobenefício pela regra dos 70%
+                if (regrados70 <= 0.7)
+                {
+                    LabelResultadoRegrados70.Text = "Pela regra dos 70% é mais Custobenefício abastecer com Álcool.";
+                }
+                else
+                {
+                    LabelResultadoRegrados70.Text = "Pela regra dos 70% é mais Custobenefício abastecer com Gasolina.";
+                }
+            }
+        }
+        }
+}
